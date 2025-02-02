46ef733cce8221dd5cb287cc4721494df771e4e3
Hikmet Çatak <144388425+HikmetCTK@users.noreply.github.com>
2025-02-02 16:48:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def connect():
         try:
             connection=pymysql.connect(host='localhost',
                                 user='root',
-                                password="sql5858",
-                                database='rcpt_mng1')
+                                password="password",
+                                database='database_name')
             return connection
         except pymysql.MySQLError as e:
             return str(e)   
